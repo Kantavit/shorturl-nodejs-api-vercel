f4158932074a4803c77151ef42d017022c094fef
Kantavit <krite-18354@hotmail.com>
2023-01-05 11:36:11 +0700

    edit package.json

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('config');
-process.env['NODE_CONFIG_DIR'] = config.join(config.resolve("./"),"config/")
-
 const db = config.get('mongoURI');
 
 const connectDB = async() => {
